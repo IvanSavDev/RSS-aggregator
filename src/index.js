e7d428bef749d5e850e7a0657090fb96e2a04962
IvanSavDev <ivan-savenko@mail.ru>
2022-05-22 14:16:35 +0300

    fead: finish 2 point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 import './scss/style.scss';
+import * as yup from 'yup';
+import render from './view';
+
+const form = document.querySelector('.rss-form');
+
+const state = {
+  valid: true,
+  processState: 'filling',
+  processError: null,
+  data: {
+    url: '',
+  },
+  feeds: [],
+};
+
+const watchedState = render(state, form);
+
+const validate = (datas, feeds) => {
+  const schema = yup.object({
+    url: yup.string().required().url()
+      .notOneOf(feeds),
+  });
+  return schema.validate(datas, { abortEarly: false })
+    .then(() => '')
+    .catch((e) => {
+      if (e.message === 'url must be a valid URL') {
+        return 'невалидный url';
+      }
+      console.log(e.message);
+      return 'уже такая ссылка есть';
+    });
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const value = data.get('url');
+  // state.processState = 'sending';
+  // state.processError = null;
+  watchedState.data.url = value;
+  validate(state.data, state.feeds)
+    .then((errors) => {
+      if (errors === '') {
+        watchedState.feeds.push(value);
+      } else {
+        watchedState.processError = `${errors}`;
+      }
+    }).catch((er) => {
+      watchedState.processState = 'error';
+      watchedState.processError = `${er}`;
+    });
+});
---
+++ src/view.js
@@ -0,0 +1,33 @@
+import onChange from 'on-change';
+
+const renderUrl = (data, form) => {
+  const input = form.querySelector('.form-control');
+  input.classList.remove('is-invalid');
+  form.reset();
+  input.focus();
+};
+
+const renderErrors = (nameError, form) => {
+  const input = form.querySelector('.form-control');
+  const textError = document.querySelector('.feedback');
+
+  textError.textContent = nameError;
+  input.classList.add('is-invalid');
+};
+
+export default (state, form) => {
+  const watchedState = onChange(state, (path, value) => {
+    switch (path) {
+      case 'feeds':
+        renderUrl(value, form);
+        break;
+      case 'processError':
+        renderErrors(value, form);
+        break;
+      default:
+        break;
+    }
+  });
+
+  return watchedState;
+};
